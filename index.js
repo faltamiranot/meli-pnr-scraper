5fa67bc239852e44a5d75756547118c9e7dfc4b8
faltamiranot <facundo.altamirano@clicoh.com>
2025-05-08 13:15:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import { parseCSV } from './utils/parseCsv.js';
+
+import { parseCSV } from './parseCsv.js';
 import { createClient } from './supabase.js';
-import fs from 'fs';
-import readline from 'readline';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const SUPABASE = createClient();
 
@@ -15,26 +16,37 @@ async function main() {
       .upsert({
         id_caso: row["ID DEL CASO"],
         fecha_caso: row["FECHA DEL CASO"],
+        periodo_facturacion: row["PERIODO DE FACTURACION"],
         tipo_pnr: row["TIPO DE PNR"],
+        fecha_pedido_revision: row["FECHA PEDIDO DE REVISION"],
         estado: row["ESTADO"],
+        pedido_revision: row["PEDIDO DE REVISION"],
+        fecha_cierre: row["FECHA DE CIERRE DE CASO"],
+        rep_asistente: row["REP  ASISTENTE"],
+        comentario_cierre: row["COMENTARIO DE CIERRE"],
+        n_prefactura: row["N DE PREFACTURA"],
         id_envio: row["ID DE ENVIO"],
         productos: row["PRODUCTOS"],
         valor_compra: parseFloat(row["VALOR DE LA COMPRA"].replace(/[$,]/g, '')),
+        rep_transportadora: row["REP TRANSPORTADORA"],
+        id_transportadora: row["ID DE TRANSPORTADORA"],
+        transportadora: row["TRANSPORTADORA"],
+        estacion_origen: row["ESTACION DE ORIGEN"],
+        ruta: row["RUTA"],
         id_conductor: row["ID DEL CONDUCTOR"],
+        fecha_entrega: row["FECHA DE ENTREGA"],
+        id_reclamo: row["ID DE RECLAMO"],
+        fecha_reclamo: row["FECHA DEL RECLAMO"],
         nombre_driver: row["Nombre Driver"],
         proveedor: row["PROVEEDOR"],
         nombre_cliente: row["Nombre Cliente"],
+        tel_1: row["Tel 1"],
         direccion_cliente: row["Direccion cliente"],
-        quien_recibio: row["Quien recibio"],
-        fecha_entrega: row["FECHA DE ENTREGA"],
-        estacion_origen: row["ESTACION DE ORIGEN"],
-        ruta: row["RUTA"],
-        id_reclamo: row["ID DE RECLAMO"],
-        comentario_cierre: row["COMENTARIO DE CIERRE"]
+        quien_recibio: row["Quien recibio"]
       }, { onConflict: 'id_caso' });
   }
 
-  console.log('Datos subidos a Supabase.');
+  console.log('✅ Datos subidos correctamente a Supabase.');
 }
 
 main();
